747fc56bbc702f6b91211fdac9fbaca54049b4fd
Rahul Tak <codebase.rahul@gmail.com>
2026-09-10 22:03:54 +0530

    function overriding

--- Polymorphism/App.java
+++ Polymorphism/App.java
@@ -1,7 +1,16 @@
 public class App {
     public static void main(String[] args) {
-        Calculator c1 = new Calculator();
 
-       System.out.println(c1.add(5, 9));
+        // Method Overloading 
+    //    Calculator c1 = new Calculator();
+
+    //    System.out.println(c1.add(5, 9));
+
+
+    // Function Overriding 
+
+    Circle ci = new Circle();
+    ci.draw();
+
     }
 }
---
+++ Polymorphism/Circle.java
@@ -0,0 +1,6 @@
+
+public class Circle extends Shape{
+    public  void draw(){
+        System.out.println("Circle is created....!");
+    }
+}
